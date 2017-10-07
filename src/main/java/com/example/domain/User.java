2966289093e2f98b2556fa43b8de998facc8a30c
Taiji Miyabe <a1153tm@aiit.ac.jp>
2017-10-08 00:19:04 +0900

    双方向関連の実装

--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -15,16 +15,14 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue
-    @Setter
-    @Getter
     private Integer id;
     private String firstName;
     private String lastName;
